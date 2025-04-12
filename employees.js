422f0f0881f9140a10fd912815bb98efcf374f16
Brandon Duong <brandon@Brandons-MacBook-Pro.local>
2025-04-12 15:23:52 -0400

    Created employees data model

---
+++ employees.js
@@ -0,0 +1,62 @@
+const employees = [
+  {
+    id: 1,
+    name: "Evan",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 2,
+    name: "Dina",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 3,
+    name: "Tina",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset", "Fill"],
+  },
+  {
+    id: 4,
+    name: "Jane",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 5,
+    name: "Kathy",
+    allowedServices: ["Pedicure", "Men Manicure"],
+  },
+  {
+    id: 6,
+    name: "Henry",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 8,
+    name: "Peter",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 9,
+    name: "Tom",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 10,
+    name: "Tony",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 11,
+    name: "Evan",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 12,
+    name: "Sophia",
+    allowedServices: ["Pedicure", "Manicure", "SNS", "Fullset"],
+  },
+  {
+    id: 13,
+    name: "Lilac",
+    allowedServices: ["Pedicure", "Men Manicure"],
+  },
+];
